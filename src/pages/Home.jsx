b9389dd40672db9df795e5ddf68e3d05d1b67006
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-21 15:59:40 +0200

    Create Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import MainLayout from "../layouts/MainLayout";
+
+function Home() {
+  return (
+    <MainLayout>
+      <h1 className="text-2xl font-semibold mb-4">
+        Explore places to stay
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {/* Cards will go here */}
+      </div>
+    </MainLayout>
+  );
+}
+
+export default Home;
